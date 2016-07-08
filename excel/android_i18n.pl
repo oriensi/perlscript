81dc9aff7a43fd69035c5c3b22f1bb1e3c74eb93
pasiel <fxdgrb@gmail.com>
2016-07-08 18:10:10 +0800

    查找文件方式修改

--- excel/android_i18n.pl
+++ excel/android_i18n.pl
@@ -1,42 +1,34 @@
 #!/usr/bin/env perl
 use File::Find;
+#use XML::Simple;
+use Data::Dumper;
 use strict;
 use warnings;
 use 5.010;
 
 my ($root, $outFile, @values) = @ARGV;
-
+$root.= "/" unless substr($root, -1) eq "/";
 say "project: $root";
 say "values: @values";
 say "write to: $outFile";
 
-my @android_dirs;
-sub find_android_dir {
-  push @android_dirs, $File::Find::dir if $_ eq "Android.mk";
-}
+my @res_dirs;
 
 sub find_res_dir {
-  my $an_dir = shift;
-  my $an_mk_file = $an_dir."/Android.mk";
-  open AMF, "<", $an_mk_file or die "open failed!";
-  my @args = stat($an_mk_file);
-  my $content = do { local $/; <AMF> };
-  my @an_res_dirs;
-  while ($content =~ /^\s*LOCAL_RESOURCE_DIR \+?=\s*?(.*?[^\\])\s*$/gms) {
-    my $tmp = $1;
-    $tmp =~ s/\Q$(LOCAL_PATH)\E/$an_dir/g;
-    say "TMP: $tmp";
-    push @an_res_dirs, (split /[\s\\]+/, $tmp);
-  }
-  @an_res_dirs;
+    if ($File::Find::dir =~ m#^.*/res[^/]*$# && $_ =~ /^values$/) {
+        opendir DIR, $File::Find::name or die "opendir err";
+        my $string = grep { /^(?:mtk_)?(strings|arrays).xml$/ && -f "$File::Find::name/$_" } readdir DIR;
+        push @res_dirs, $File::Find::dir if $string;
+    }
 }
 
-find (\&find_android_dir, "$root"."packages/apps");
+find(\&find_res_dir, "$root"."packages/apps");
+say foreach (@res_dirs);
 
-for (@android_dirs) {
-  my @res_dirs = find_res_dir($_);
-  if (@res_dirs > 0) {
-    say "#############print res dirs#############";
-    say foreach (@res_dirs);
-  }
-}
+#for (@res_dirs) {
+my $file = shift @res_dirs;
+$file.="/values/mtk_strings.xml";
+say "FILE: ", $file;
+# XML::Simple not a good choise
+print Dumper $xml;
+#}
