ba8e706edd971dc3f6fe8c8ce699ff35b6e043aa
pasiel <fxdgrb@gmail.com>
2016-09-14 18:45:25 +0800

    export strings

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ LWP表单，对于可选的上传文件项与使用浏览器时不一样。
 使用浏览器时的request可以看到 filename=""这样的内容，
 而使用脚本时没有此项内容: ./LWP/MRequest.pm
 
+android 字符串导出: ./excel/android_i18n.pl
+
 ```
 # LWP
 perl -MCPAN -e 'install Bundle::LWP'
--- excel/android_i18n.pl
+++ excel/android_i18n.pl
@@ -8,14 +8,19 @@ use strict;
 use warnings;
 use 5.010;
 
-my ($root, $outFile, @values) = @ARGV;
+my ($root, $overlay_dir, $find_path, $outFile, @values) = @ARGV;
 $root.= "/" unless substr($root, -1) eq "/";
+$overlay_dir.= "/" unless substr($overlay_dir, -1) eq "/";
+$find_path.= "/" unless substr($find_path, -1) eq "/";
 @values = sort @values;
 say "project: $root";
 say "values: @values";
 say "write to: $outFile";
+say "overlay dir: $overlay_dir";
+say "module path: $find_path";
 
 my @res_files;
+my $module_resdir_ref;
 
 my $workbook = Spreadsheet::WriteExcel->new($outFile);
 my $worksheet = $workbook->add_worksheet();
@@ -45,22 +50,29 @@ sub parse_xml {
   my $parser = XML::LibXML->new;
   for my $file (@$files) {
     my $value_dir;
-    if ($file =~ m#/(values(?:-[^/]*)?)/#) {
+    if ($file =~ m{/(values(?:-[^/]*)?)/}) {
       $value_dir = $1;
     } else {
       say "sth is wrong!!";
     }
     my $doc = $parser->parse_file($file) or die "can't parse $file";
     my @nodeList = $doc->getElementsByTagName('string');
+    my @arrayNodes = $doc->getElementsByTagName('string-array');
     for my $node (@nodeList) {
       my @attributelist = $node->attributes();
       my $value = $node->innerXML;
       my ($name) = map { $_->nodeValue if $_->nodeName =~ /name/ } @attributelist;
-      say "name: ".$name."    value: ".$value;
+      # say "name: ".$name."    value_dir:".$value_dir."    value: ".$value;
+      $name_value{$name}{$value_dir} = $value;
+    }
+    for my $node (@arrayNodes) {
+      my @attributelist = $node->attributes();
+      my ($name) = map { $_->nodeValue if $_->nodeName =~ /name/ } @attributelist;
+      # my @childNodes = $node->childNodes;
+      my $value = join "\n", map {$_->innerXML} $node->childNodes;
       $name_value{$name}{$value_dir} = $value;
     }
   }
-  # print Dumper %name_value;
   \%name_value;
 }
 
@@ -81,45 +93,44 @@ sub write_to_xls {
   }
 }
 
-sub resdir2module {
+sub resfiles2module {
   my $files = shift;
-  my %module_resdir;
+  # my %module_resdir;
   for my $file (@$files) {
     my $module;
     say $file;
-    if ($file =~ m#$root(.*)/res(?:_[^/]*)?/values#) {
+    if ($file =~ m#$root(?:$overlay_dir)?(.*)res(?:_[^/]*)?/values#) {
       $module = $1;
-      # say 'module: '.$module.'    file:'.$file;
     } else {
       say 'oh no!!!';
       next;
     }
-    my @path = $module_resdir{$module} ? $module_resdir{$module} : ();
-    if (@path) {
-      push $module_resdir{$module}, $file;
+    if ($module_resdir_ref->{$module}) {
+      push $module_resdir_ref->{$module}, $file;
     } else {
-      $module_resdir{$module} = [$file];
+      $module_resdir_ref->{$module} = [$file];
     }
   }
-  # print Dumper %module_resdir;
-  \%module_resdir;
 }
 
-find(\&find_res_dir, "$root"."packages/apps");
+sub module_reslist {
+  my ($root, $overlay_dir, $path) = @_;
+  my @directoies = ( $root.$path, $root.$overlay_dir.$path);
+  find(\&find_res_dir, @directoies);
+  resfiles2module \@res_files;
+}
 
-@res_files = sort @res_files;
-# exit;
-my $module_resdir_ref;
-$module_resdir_ref = resdir2module \@res_files;
-#exit;
+
+################### MAIN ########################################
+module_reslist $root, $overlay_dir, $find_path;
+# print Dumper $module_resdir_ref;
 
 die "need out file name" unless $outFile;
 
 my %parserxml_ref;
  for my $module (keys %$module_resdir_ref) {
    $parserxml_ref{$module} = parse_xml($module, $module_resdir_ref->{$module});
-   print Dumper %parserxml_ref;
-   last;
+   # print Dumper %parserxml_ref;
  }
 
 $worksheet->write(0, 0, "path");
